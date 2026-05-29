3485bf3a88b27678e6ea74077aa4aad2d478b7bc
Aniket kumar singh <aniket70045@gmail.com>
2026-05-29 13:33:26 +0530

    jenkins file uploaded

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 }   );      
 
+app.get('lala',(req,res)=>{
+    res.send('This is lala route');             
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 }); 
